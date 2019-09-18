2393f46ab2d4a90d215a64890994e2608ffce3bf
xianzhixianzhixian <MrRobotFan@163.com>
2019-09-18 23:12:41 +0800

    SpringBoot2.0整合mybatis,将mapper.xml文件放在包中而不是resources文件夹下 classpath代表的是resources文件夹,classpath*代表的是src/main/java文件夹

--- springboot-mybatis/src/main/java/com/springboot/mapper/CourseMapper.java
+++ springboot-mybatis/src/main/java/com/springboot/mapper/CourseMapper.java
@@ -2,12 +2,9 @@ package com.springboot.mapper;
 
 import com.springboot.bean.Course;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface CourseMapper {
 
-    @Select("select * from course where cno = #{cno}")
-    public Course selectByCno(@Param("cno") Integer cno);
+    Course selectByCno(Integer cno);
 }
