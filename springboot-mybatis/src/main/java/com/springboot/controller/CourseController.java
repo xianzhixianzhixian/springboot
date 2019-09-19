a83dcc46415115430762c5d1ff2f4b7bd5950bcc
xianzhixianzhixian <MrRobotFan@163.com>
2019-09-19 23:35:40 +0800

    SpringBoot2.0整合pageHelper

--- springboot-mybatis/src/main/java/com/springboot/controller/CourseController.java
+++ springboot-mybatis/src/main/java/com/springboot/controller/CourseController.java
@@ -1,10 +1,14 @@
 package com.springboot.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.springboot.bean.Course;
 import com.springboot.mapper.CourseMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CourseController {
@@ -13,7 +17,15 @@ public class CourseController {
     private CourseMapper courseMapper;
 
     @GetMapping("/course")
-    public Course getCourse(Integer cno) {
+    public List<Course> getCourse(Integer cno) {
         return courseMapper.selectByCno(cno);
+    }
+
+    @GetMapping("/pagecourse")
+    public PageInfo<Course> getCourseByPage(Integer cno, Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<Course> courses = courseMapper.selectByCno(cno);
+        PageInfo<Course> pageInfo = new PageInfo<>(courses);
+        return pageInfo;
     }
 }
--- springboot-mybatis/src/main/java/com/springboot/mapper/CourseMapper.java
+++ springboot-mybatis/src/main/java/com/springboot/mapper/CourseMapper.java
@@ -3,8 +3,10 @@ package com.springboot.mapper;
 import com.springboot.bean.Course;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface CourseMapper {
 
-    Course selectByCno(Integer cno);
+    List<Course> selectByCno(Integer cno);
 }
